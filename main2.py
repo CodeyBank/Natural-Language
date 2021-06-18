d86a837625d21b3b5a0089b08656e9d7c3bd2154
Ezeh Stanley Okechukwu <ezeh.stanley.es@gmail.com>
2021-06-18 17:51:04 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -39,13 +39,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.datasets import make_classification
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
-# import mlxtend
 from mlxtend.classifier import OneRClassifier
-from sklearn.tree import DecisionTreeRegressor
 from sklearn import tree
 from sklearn.neural_network import MLPClassifier
 
@@ -59,7 +56,7 @@ class NLToolBox:
         self.filepath = filepath
         self.X_train, self.X_test, self.y_train, self.y_test = self.loadDataSet(self.filepath)
         self.results = []
-        
+
     def loadDataSet(self, filepath):
         """
         To load dataset, provide the absolute filepath as parameter
@@ -107,7 +104,8 @@ class NLToolBox:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
         return X_train, X_test, y_train, y_test
 
-    def saveModel(self, classifier, name):
+    @staticmethod
+    def saveModel(classifier, name):
         filename = '{}_classifier_Model'.format(name)
         with open(filename, 'wb') as picklefile:
             pickle.dump(classifier, picklefile)
@@ -126,67 +124,45 @@ class NLToolBox:
 
         print('Random Forest Classifier')
 
-        print("*****  Confusion Matrix ******* \n")
-        print(confusion_matrix(self.y_test, y_pred))
-
-        print("*****  Classification report ******* \n")
-        print(classification_report(self.y_test, y_pred))
-
-        print("*****  Accuracy Score ******* \n")
-        print(accuracy_score(self.y_test, y_pred))
-
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(classifier, self.X_train, self.y_train, cv=10)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        # Display results
+        cv_scores = self._repr(classifier, y_pred)
 
         # Save the file
         self.saveModel(classifier, name)
-        
+
         # Append value to the results
         self.results.append((name, cv_scores.mean()))
-        return cv_scores
+        return
 
     def KNeighbors(self):
         """
             Implements the KNeighbors classifier
         """
-        
+
         name = "KNeighbors"
         knc = KNeighborsClassifier(n_neighbors=4)
         knc.fit(self.X_train, self.y_train)
-        score = knc.score(self.X_train, self.y_train)
+
         y_pred = knc.predict(self.X_test)
 
-        cm = confusion_matrix(self.y_test, y_pred)
         print("K-Neighbours Classifier")
-        print("Training score: ", score)
 
-        print("*****  Confusion Matrix ******* \n")
-        print(cm)
-
-        print("*****  Classification report ******* \n")
-        print(classification_report(self.y_test, y_pred))
-
-        print("*****  Accuracy Score ******* \n")
-        print(accuracy_score(self.y_test, y_pred))
-
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(knc, self.X_train, self.y_train, cv=10)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        # Display results
+        cv_scores = self._repr(knc, y_pred)
 
         # Save the file
         self.saveModel(knc, name)
-        
+
         # Append value to the results
         self.results.append((name, cv_scores.mean()))
-        
-        return cv_scores
+
+        return
 
     def naiveBayes(self):
         """
             Implements the naive bayes classifier
         """
-        
+
         # Initialize our classifier
         name = "NaiveBayes"
         gnb = GaussianNB()
@@ -198,27 +174,17 @@ class NLToolBox:
         preds = gnb.predict(self.X_test)
 
         print("*****  Naive Bayes Classifier ******* \n")
-        print(accuracy_score(self.y_test, preds))
 
-        report = classification_report(self.y_test, preds)
-        print("*****  Classification report ******* \n")
-        print(report)
-
-        cmatrix = confusion_matrix(self.y_test, preds)
-        print("*****  Confusion Matrix ******* \n")
-        print(cmatrix)
-
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(model, self.X_train, self.y_train, cv=10)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        # Display results
+        cv_scores = self._repr(model, preds)
 
         # Save the file
         self.saveModel(model, name)
-        
+
         # Append value to the results
         self.results.append((name, cv_scores.mean()))
-        
-        return cv_scores
+
+        return
 
     def svcClassifier(self):
         """
@@ -227,28 +193,22 @@ class NLToolBox:
         name = 'SVC'
         svc = SVC()
         svc.fit(self.X_train, self.y_train)
-        score = svc.score(self.X_train, self.y_train)
+        # score = svc.score(self.X_train, self.y_train)
 
         print("*****  SVC Classifier ******* \n")
-        print("Score: ", score)
 
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(svc, self.X_train, self.y_train, cv=10)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        y_pred = svc.predict(self.X_test)
 
-        ypredicted = svc.predict(self.X_test)
-
-        cm = confusion_matrix(self.y_test, ypredicted)
-        print("*****  Confusion Matrix ******* \n")
-        print(cm)
+        # Display results
+        cv_scores = self._repr(svc, y_pred)
 
         # Save the model
         self.saveModel(svc, name)
-        
+
         # Append value to the results
         self.results.append((name, cv_scores.mean()))
-        
-        return cv_scores
+
+        return
 
     def one_RClassifier(self):
         name = "O_R"
@@ -257,7 +217,7 @@ class NLToolBox:
         """
         oner = OneRClassifier()
 
-        oner.fit(self.X_train, self.y_train);
+        oner.fit(self.X_train, self.y_train)
         y_prediction = oner.predict(self.X_train)
         train_acc = np.mean(y_prediction == self.y_train)
 
@@ -265,61 +225,41 @@ class NLToolBox:
         print(f'Training accuracy: \n  {train_acc * 100:.2f}%')
         y_pred = oner.predict(self.X_test)
         test_acc = np.mean(y_pred == self.y_test)
-        print(f'Test accuracy {test_acc * 100:.2f}%')
+        print(f'Test accuracy {test_acc * 100:.2f}% \n')
 
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(oner, self.X_train, self.y_train, cv=10)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        # Display results
+        cv_scores = self._repr(oner, y_pred)
 
-        cm = confusion_matrix(self.y_test, y_pred)
-        print("*****  Confusion Matrix ******* \n")
-        print(cm)
-        
-        
-         # Save the model
+        # Save the model
         self.saveModel(oner, name)
-        
+
         # Store the values of the results
         self.results.append((name, cv_scores.mean()))
         return name
 
     def decisionTreeClassifier(self):
-        
+
         """
             Implements the decision tree classifier
         """
         name = "DT"
         model = tree.DecisionTreeClassifier()
-        # create a regressor object
-        regressor = DecisionTreeRegressor(random_state=0)
-
-        # fit the regressor with X and Y data
-        regressor.fit(self.X_train, self.y_train)
 
         model.fit(self.X_train, self.y_train)
-
-        y_predict = model.predict(self.X_test)
-        score = accuracy_score(self.y_test, y_predict)
+        y_pred = model.predict(self.X_test)
 
         print("*****  Decision Tree Classifier ******* \n")
-        print("Score: ", score)
 
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(model, self.X_train, self.y_train, cv=8)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        # Display results
+        cv_scores = self._repr(model, y_pred)
 
-        cm = confusion_matrix(self.y_test, y_predict)
-        print("*****  Confusion Matrix ******* \n")
-        print(cm)
-        
         # Append value to the results
         self.results.append((name, cv_scores.mean()))
-        
+
         # Save the model
         self.saveModel(model, name)
-        
-        return 
-    
+        return
+
     def neuralNetClassifier(self):
         """
             Artificial Neural Network classifier using multilayer perceptrons
@@ -327,111 +267,116 @@ class NLToolBox:
             Implementation solver uses lbfgs solver
             alpha is set to 1e-5, and 6 hidden layers
         """
-        
         name = "NN"
-        clf = MLPClassifier(solver='lbfgs', 
-                    alpha=1e-5,
-                    hidden_layer_sizes=(6,), 
-                    random_state=1)
+        clf = MLPClassifier(solver='lbfgs',
+                            alpha=1e-5,
+                            hidden_layer_sizes=(6,),
+                            random_state=1)
 
         clf.fit(self.X_train, self.y_train)
-        
-        y_predict = clf.predict(self.X_test)
-        score = accuracy_score(self.y_test, y_predict)
 
-        print("*****  Neural Network Classifier ******* \n")
-        print("Score: ", score)
+        y_pred = clf.predict(self.X_test)
 
-        ## Cross-validaton  using 10-fold cross validation
-        cv_scores = cross_val_score(clf, self.X_train, self.y_train, cv=10)
-        print("Cross-Validation average score: %.2f \n" % cv_scores.mean())
+        # Display results
+        cv_scores = self._repr(clf, y_pred)
 
-        cm = confusion_matrix(self.y_test, y_predict)
-        print("*****  Confusion Matrix ******* \n")
-        print(cm)
-        
         # Append value to the results
         self.results.append((name, cv_scores.mean()))
-        
+
         # Save the model
         self.saveModel(clf, name)
-        
+
         return cv_scores
-        
-        
-        
+
     def testModel(self, modelpath, datapath):
         """
-           This method is used for testing the generated models and 
+           This method is used for testing the generated models and
            comparing with training results.
            @params:
             - modelpath: is the absolute file path to the model
             - datapath: path to the dataset to be tested
         """
-        
+
         with open(modelpath, 'rb') as training_model:
             model = pickle.load(training_model)
-            
+
         X_train, X_test, y_train, y_test = self.loadDataSet(datapath)
 
         y_pred2 = model.predict(X_test)
-        
+
         print("*****  Confusion Matrix ******* \n")
         print(confusion_matrix(y_test, y_pred2))
-        
+
         print("*****  Classification report ******* \n")
         print(classification_report(y_test, y_pred2))
 
         print("*****  Accuracy ******* \n")
         print(accuracy_score(y_test, y_pred2))
-        
+
         return
-        
-    
+
+    def _repr(self, model, y_pred):
+
+        # confusion matrix
+        cm = confusion_matrix(self.y_test, y_pred)
+
+        # Classification report
+        report = classification_report(self.y_test, y_pred)
+
+        # Cross-Validation score
+        cv_scores = cross_val_score(model, self.X_train, self.y_train, cv=10)
+
+        # Display results
+        print(" --------  Confusion Matrix ----------- \n {} \n".format(cm))
+        print("--------- Classification report --------- \n {} \n".format(report))
+        print("--------- Cross Validation score --------- \n {} \n".format(cv_scores.mean()))
+
+        return cv_scores
+
     def Plot(self):
         """
-            Create a box plot of all algorthims for testing
+            Create a box plot of all algorithms for testing
         """
         names = []
         results = []
-        
-        # Save all the names and results in the arrays 
-        if len(self.results) > 0:
+
+        # Save all the names and results in the arrays
+        if len(self.results) > 1:
             for name, result in self.results:
                 names.append(name)
-                results.append(result)  
-        
-            # Plot algorithm comparison
+                results.append(result)
+
+                # Plot algorithm comparison
             fig = plt.figure()
             fig.suptitle('Algorithm Comparison')
-            
-            plt.bar(names,results, color='orange', width=0.1)     
-            plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.7)
+
+            plt.bar(names, results, color='orange', width=0.8)
+            plt.grid(color='#95a5a6', linestyle='--', linewidth=1, axis='y', alpha=0.7)
             plt.xlabel('Classifier')
             plt.ylabel('Cross validation score')
             plt.show()
-            
+
         else:
-            print("You must generate at least one classifier to plot")
-            
+            print("You must generate at least two classifiers for comparison plot")
+
         return
-        
+
+
 if __name__ == '__main__':
     try:
         dataset_path = sys.argv[1]
-        #w = NLToolBox(r"TextClass_text")
+        # w = NLToolBox(r"TextClass_text")
         w = NLToolBox(dataset_path)
-        
+
         # Extract the command
         command = sys.argv[2] if len(sys.argv) > 1 else None
-
+        command = command.lower()
 
         if command == "-t":
             modelpath = sys.argv[3] if len(sys.argv) > 2 else None
             w.testModel(modelpath, dataset_path)
-                     
+
         if command == "-rs":
-            print(sys.argv[2:])
             # run the selected classifiers in the command from index 2 to end
             for argument in sys.argv:
                 if argument == "DT":
@@ -447,13 +392,14 @@ if __name__ == '__main__':
                 if argument == "KN":
                     w.KNeighbors()
                 if argument == "RF":
-                    w.randomForest() 
+                    w.randomForest()
             w.Plot()
         else:
-            print("""Please pass the right command first.\n Use 'rs' to run selected classifier algorithms or use 'test' to test a model""")
-            
+            print(
+                """Please pass the right command first.\n Use '-rs' to run selected classifier algorithms or use '-t' to test a model""")
+
     except (TypeError, FileNotFoundError):
-        error= """
+        error = """
         Please Select a classifier to run \n
         - KN - K-Neighbors \n
         - DT - Decision Tree \n
@@ -462,12 +408,11 @@ if __name__ == '__main__':
         - NN - Neural Network \n
         - NB - Naive Bayes  \n    
         - RF - Random Forest \n
-        
+
         To test a model with another dataset use the following
         python main.py test <model_path> <dataset_path> 
-        
+
         Plot method plots the selected algorithm cross validation scores
         """
         print(error)
-        
-        
+
